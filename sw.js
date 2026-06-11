d05c8a477642ad23496a765acd7450374b1f1a27
Wystan Hung <wystan0211@gmail.com>
2026-06-11 20:45:43 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,4 @@
-const CACHE = 'bor-v2';
-const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
-});
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(cached => {
-    if(cached) return cached;
-    return fetch(e.request).then(resp => {
-      if(!resp||resp.status!==200||resp.type==='opaque') return resp;
-      const clone=resp.clone();
-      caches.open(CACHE).then(c=>c.put(e.request,clone));
-      return resp;
-    }).catch(()=>caches.match('./index.html'));
-  }));
-});
+const CACHE='bor-v3';const ASSETS=['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(cached=>{if(cached)return cached;return fetch(e.request).catch(()=>caches.match('./index.html'));}));});
